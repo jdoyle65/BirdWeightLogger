5611f9139a39dc002e9e34494e80f46e956628eb
Justin Doyle <jdoyle65@uwo.ca>
2014-03-24 00:08:50 -0400

    Wrote javadoc for ConfigParser.java

--- src/com/jdoyle65/birdwl/ConfigParser.java
+++ src/com/jdoyle65/birdwl/ConfigParser.java
@@ -7,6 +7,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Properties;
 
+/**
+ * Used to get configuration settings for the Bird Weight Logger.
+ * @author Justin Doyle
+ *
+ */
 public class ConfigParser {
 	private Properties props;
 	private int bridges;
@@ -18,28 +23,72 @@ public class ConfigParser {
 	private HashMap<Integer, Integer> rfid_map;
 	private HashMap<Integer, Integer> bridge_map;
 	
+	/**
+	 * Default constructor.
+	 * @param fileName The configuration file name.
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public ConfigParser(String fileName) throws FileNotFoundException, IOException {
 		props = new Properties();
 		props.load(new FileInputStream(new File(fileName)));
 		getProperties();
 	}
 	
+	/**
+	 * Get the number of RFIDs supposed to be attached.
+	 * @return The number of RFID readers supposed to be attached.
+	 */
 	public int getNumRfidReaders() { return rfid_readers; }
+	
+	/**
+	 * Get the number of Bridges supposed to be attached.
+	 * @return The number of Bridges supposed to be attached.
+	 */
 	public int getNumBridges() { return bridges; }
+	
+	/**
+	 * Get an array of the serial numbers of attached RFIDs
+	 * @return Array of RFID serial numbers.
+	 */
 	public int[] getRfidSerials() { return rfid_serials; }
+	
+	/**
+	 * Get an array of the Bridge serial numbers attached.
+	 * @return Array of Bridge serial numbers.
+	 */
 	public int[] getBridgeSerials() { return bridge_serials; }
+	
+	
+	/**
+	 * Get the serial number for the associated RFID index.
+	 * @param i The index of the RFID.
+	 * @return The serial number for given RFID index.
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
 	public int getRfidSerial(int i) throws ArrayIndexOutOfBoundsException {
 		if(i >= rfid_readers)
 			throw new ArrayIndexOutOfBoundsException("RFID Serial index out of bounds\n");
 		return rfid_serials[i];
 	}
 	
+	/**
+	 * Get the serial number for the associated Bridge index.
+	 * @param i The index of the Bridge.
+	 * @return The serial number for given Bridge index.
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
 	public int getBridgeSerial(int i) throws ArrayIndexOutOfBoundsException {
 		if (i >= bridges)
 			throw new ArrayIndexOutOfBoundsException("Bridge Serial index out of bounds\n");
 		return bridge_serials[i];
 	}
 	
+	/**
+	 * Get the Bridge paired with the given RFID reader.
+	 * @param serial The serial number for the RFID reader.
+	 * @return The index number for the paired Bridge. Returns -1 if no such pairing.
+	 */
 	public int getRfidBridge(int serial) {
 		Integer i = rfid_map.get(serial);
 		if(i == null)
@@ -47,6 +96,11 @@ public class ConfigParser {
 		return rfid_pairings[i].getBridge();
 	}
 	
+	/**
+	 * Get the Load Cell paired with the given RFID reader.
+	 * @param serial The serial number for the RFID reader.
+	 * @return The index number for the paired Load Cell. Returns -1 if no such pairing.
+	 */
 	public int getRfidLoadCell(int serial) {
 		Integer i = rfid_map.get(serial);
 		if(i == null)
@@ -54,15 +108,31 @@ public class ConfigParser {
 		return rfid_pairings[i].getLoadCell();
 	}
 	
+	/**
+	 * Get the rate in milliseconds at which to capture Bridge data.
+	 * @return The rate in milliseconds.
+	 */
 	public int getDataRate() {
 		return data_rate;
 	}
 	
+	/**
+	 * Get the offset for a specific Load Cell.
+	 * @param bridge The Bridge index the cell is attached to.
+	 * @param input The Load Cell index.
+	 * @return The cell's offset (positive or negative).
+	 */
 	public double getLoadCellOffset(int bridge, int input) {
 		String offset = props.getProperty("offset_" + bridge + "_" + input, "0");
 		return Double.parseDouble(offset);
 	}
 	
+	/**
+	 * Get the K value for a specific Load Call.
+	 * @param bridge The Bridge index the cell is attached to.
+	 * @param input The Load Cell index.
+	 * @return The cell's K value.
+	 */
 	public double getLoadCellKValue(int bridge, int input) {
 		String k = props.getProperty("k_" + bridge + "_" + input, "1");
 		return Double.parseDouble(k);
@@ -78,16 +148,19 @@ public class ConfigParser {
 		bridge_map = new HashMap<Integer, Integer>(bridges);
 		data_rate = Integer.parseInt(props.getProperty("data_rate", "500"));
 		
+		// Get all the bridges and their associated indexes.
 		for(int i = 0; i < bridges; i++) {
 			bridge_serials[i] = Integer.parseInt(
 					props.getProperty("bridge_" + i, "-1"));
 			bridge_map.put(bridge_serials[i], i);
 		}
+		// Get all rfids and their associated indexes.
 		for(int i = 0; i < rfid_readers; i++) {
 			rfid_serials[i] = Integer.parseInt(
 					props.getProperty("rfid_" + i, "-1"));
 			rfid_map.put(rfid_serials[i], i);
 		}
+		// Get all rfids' Bridge and Load Cell pairings
 		for(int i = 0; i < rfid_readers; i++) {
 			String p = props.getProperty("rfid_" + i + "_pair");
 			String[] vals = p.split("-");
